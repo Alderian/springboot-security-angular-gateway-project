8cb32da5fa3ce8a6029ae9ea5ca73ed1a247084b
alderian <alderian@msn.com>
2015-09-18 00:04:25 -0300

    Use of roles in UI application

--- ui/src/main/resources/static/js/hello.js
+++ ui/src/main/resources/static/js/hello.js
@@ -7,7 +7,8 @@ function($scope, $http) {
 	$http.get('user').success(function(data) {
 		if (data.name) {
 			$scope.authenticated = true;
-			$scope.user = data.name
+			$scope.user = data.name;
+			$scope.roles = data.roles;
 			$http.get('/resource/').success(function(data) {
 				$scope.greeting = data;
 			})
@@ -17,5 +18,12 @@ function($scope, $http) {
 	}).error(function() {
 		$scope.authenticated = false;
 	});
+
+	$scope.isAuthorized = function(role) {
+		return $scope.roles.indexOf(role) > 0;
+	};
+
+
+	console.log('Done')
 
 });
